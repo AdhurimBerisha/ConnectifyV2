b8b6ae9968f453341da5d96534a790a628202ecb
AdhurimBerisha <138231376+AdhurimBerisha51865@users.noreply.github.com>
2024-01-07 01:57:44 +0100

    Post Card and Comments finished

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,5 +4,6 @@ import CustomButton from "./CustomButton";
 import TopBar from "./TopBar";
 import ProfileCard from "./ProfileCard";
 import FriendsCard from "./FriendsCard";
+import PostCard from "./PostCard";
 
-export { Loading, TextInput, CustomButton, TopBar, ProfileCard, FriendsCard }
+export { Loading, TextInput, CustomButton, TopBar, ProfileCard, FriendsCard, PostCard }
